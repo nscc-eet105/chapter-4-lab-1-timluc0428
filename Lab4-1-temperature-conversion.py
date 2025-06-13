1002577c05796b67a07f7a31edb9ce12f3cdde9e
Tim Lucas <tlucas@northweststate.edu>
2025-06-13 15:38:09 -0400

    assignment complete

--- Lab4-1-temperature-conversion.py
+++ Lab4-1-temperature-conversion.py
@@ -9,30 +9,37 @@
 #if an invalid value is entered an error message should be displayed
     #and the user should be prompted to enter valid date 
 #program should not end until valid input entered.
-def main()
-    
-    temperature = int(input("What is the current temperature? "))
-    f_or_c = input("Is the temperature Celsius or Fahrenheit (c or f): ")
-    While True:
-        if f_or_c.lower() = "f":
-            fahrenheit_to_celsius()
-                ...
-        elif: f_or_c = "c"
-            celsius_to_fahrenheit(temperature)
-                ...
-        else:
-            print("Please enter a c or f")
-        
-#void function has Celsius as argument passed to it and converts to Fahrenheit 
-#and prints value
-def celsius_to_fahrenheit(temp)
+
+def celsius_to_fahrenheit(temp):
     ftemp = (temp * 9/5) + 32
-    print(ftemp)
+    print(f"The current temperature is {temp:.1f} celsius. This temperature in Fahrenheit is {ftemp:.2f} degrees")
     
 #value returning function that has Fahrenheait passed to it and and converts to Celsius
 # and returns its value   
-def fahrenheit_to_celsius()
+def fahrenheit_to_celsius(temp):
+    ctemp = (temp - 32) * 5/9
+    return ctemp
 
+
+def main():
+    
+    temperature = float(input("What is the current temperature? "))
+    while True:
+        f_or_c = input("Did you enter the temperature in (c)elsius or (f)ahrenheit? ")
+        if f_or_c.lower() == "f":
+            celtemp = fahrenheit_to_celsius(temperature)
+            print(f"The current temperature is {temperature:.1f} fahrenheit. This temperature in celsius is {celtemp:.2f} degrees.")
+            break
+    
+        elif f_or_c == "c":
+            celsius_to_fahrenheit(temperature)            
+            break
+    
+        else:
+            print("Invalid input, Please enter a c or f")
+        
+#void function has Celsius as argument passed to it and converts to Fahrenheit 
+#and prints value
 
 main()
 
